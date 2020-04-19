b6645e6f08208fb95b61a55edc6d4b20d6b1400e
HTsuruo <tsuru.dev@gmail.com>
2020-04-19 21:26:14 +0900

    feat: 簡易なコンポネント要素を配置.

---
+++ src/components/Form.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form action="">
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" name="title" className="input-form" />
+        <input type="submit" value="submit" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
