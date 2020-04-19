6d45bb54b9598e1b95d3431c8d48ef81be9a788e
HTsuruo <tsuru.dev@gmail.com>
2020-04-19 22:10:51 +0900

    feat: todo的なのを製作中.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Form from "./components/Form";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import List from "./components/List";
 
-class App extends React.Component {
+interface Props {
+  todos?: List;
+}
+interface State {}
+
+class App extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: "Hello, React!",
+          desc: "React始めました",
+          done: false,
+        },
+        {
+          id: 2,
+          title: "Hello, Redux!",
+          desc: "Reduxも始めました",
+          done: false,
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div>
         <Nav />
-        <List />
+        <h1>TODOアプリ作ってみた</h1>
+        <List todos={this.props.todos} />
         <Form />
         <Footer />
       </div>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const List = () => {
-  return <div>これはリストです</div>;
-};
+// const List = (todos) => {
+//   return <div>これはリストです</div>;
+// };
+
+interface Props {
+  todos?: List;
+}
+interface State {}
+
+class List extends React.Component<Props, State> {
+  render() {
+    const todos = this.props.todos;
+    return (
+      <>
+        <ul>{todos}</ul>
+      </>
+    );
+  }
+}
 
 export default List;
