2e714da6519a8c5e02d678c9338eaf669398c569
HTsuruo <tsuru.dev@gmail.com>
2020-04-21 22:21:42 +0900

    chore: class compoenntをfunction componentに変更.

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,26 +1,37 @@
 import React from "react";
 import Todo from "./Todo";
 
-// const List = (todos) => {
-//   return <div>これはリストです</div>;
-// };
-
 interface Props {
   todos: any[];
 }
-interface State {}
 
-class List extends React.Component<Props, State> {
-  render() {
-    const todos = this.props.todos.map((todo) => (
-      <Todo key={todo.id} {...todo} />
-    ));
-    return (
-      <>
-        <ul>{todos}</ul>
-      </>
-    );
-  }
-}
+// Function Componentで実装する場合.
+const List: React.FC<Props> = (props) => {
+  const todos = props.todos.map((todo) => <Todo key={todo.id} {...todo} />);
+  return (
+    <>
+      <ul>{todos}</ul>
+    </>
+  );
+};
+
+// Class Componentで実装する場合.
+// interface Props {
+//   todos: any[];
+// }
+// interface State {}
+
+// class List extends React.Component<Props, State> {
+//   render() {
+//     const todos = this.props.todos.map((todo) => (
+//       <Todo key={todo.id} {...todo} />
+//     ));
+//     return (
+//       <>
+//         <ul>{todos}</ul>
+//       </>
+//     );
+//   }
+// }
 
 export default List;
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -7,19 +7,32 @@ interface Props {
   desc: string;
 }
 
-class Todo extends React.Component<Props> {
-  render() {
-    const className = "undone";
-    const link = this.props.done ? "もとに戻す" : "完了！";
-    return (
-      <li className={className}>
-        <span>{this.props.id}</span>
-        <span>{this.props.title}</span>
-        <a href="#">{link}</a>
-        <p>{this.props.desc}</p>
-      </li>
-    );
-  }
-}
+const Todo: React.FC<Props> = (props) => {
+  const className = "undone";
+  const link = props.done ? "もとに戻す" : "完了！";
+  return (
+    <li className={className}>
+      <span>{props.id}</span>
+      <span>{props.title}</span>
+      <a href="#">{link}</a>
+      <p>{props.desc}</p>
+    </li>
+  );
+};
+
+// class Todo extends React.Component<Props> {
+//   render() {
+//     const className = "undone";
+//     const link = this.props.done ? "もとに戻す" : "完了！";
+//     return (
+//       <li className={className}>
+//         <span>{this.props.id}</span>
+//         <span>{this.props.title}</span>
+//         <a href="#">{link}</a>
+//         <p>{this.props.desc}</p>
+//       </li>
+//     );
+//   }
+// }
 
 export default Todo;
