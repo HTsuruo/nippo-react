d8410b4fd89a4346c47df671b0c325888bf647ea
HTsuruo <tsuru.dev@gmail.com>
2020-04-22 10:55:53 +0900

    chore: hooksを活用.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Nav from "./components/Nav";
@@ -6,45 +6,100 @@ import Footer from "./components/Footer";
 import List from "./components/List";
 
 interface Props {}
-interface State {
-  todos: any[];
-  countTodo: number;
-}
+// interface State {
+//   todos: any[];
+//   countTodo: number;
+//   count: number;
+// }
 
-class App extends React.Component<Props, State> {
-  constructor(props: any) {
-    super(props);
-    const todos = [
-      {
-        id: 1,
-        title: "Hello, React!",
-        desc: "React始めました",
-        done: false,
-      },
-      {
-        id: 2,
-        title: "Hello, Redux!",
-        desc: "Reduxも始めました",
-        done: false,
-      },
-    ];
-    this.state = {
-      todos: todos,
-      countTodo: todos.length + 1,
-    };
-  }
+// function componentで記述する
+const App: React.FC<Props> = (props) => {
+  const [count, setCount] = useState(0);
+  const [inputVal, setinputVal] = useState("");
+  const [todos, settodos] = useState([""]);
 
-  render() {
-    return (
-      <div>
-        <Nav />
-        <h1>TODOアプリ作ってみた</h1>
-        <Form />
-        <List todos={this.state.todos} />
-        <Footer />
-      </div>
-    );
-  }
-}
+  const todosNode = todos.map((todo, idx) => {
+    return <li key={idx}>{todo}</li>;
+  });
+
+  const onsubmit = () => {
+    settodos([inputVal]);
+    setinputVal("");
+  };
+
+  const onchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setinputVal(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>TODOアプリ作ってみた</h1>
+      <form action="" onSubmit={() => onsubmit()}>
+        <p>{inputVal}</p>
+        <input type="text" value={inputVal} onChange={(e) => onchange(e)} />
+        <input type="submit" value="送信" />
+      </form>
+      <ul>{todosNode}</ul>
+
+      <h1>ただのカウンター</h1>
+      <div>you clicked {count} times</div>
+      <button onClick={() => setCount(count + 1)}>プラス</button>
+      <button onClick={() => setCount(count - 1)}>マイナス</button>
+    </div>
+  );
+};
+
+// class component
+// class App extends React.Component<Props, State> {
+//   constructor(props: any) {
+//     super(props);
+//     const todos = [
+//       {
+//         id: 1,
+//         title: "Hello, React!",
+//         desc: "React始めました",
+//         done: false,
+//       },
+//       {
+//         id: 2,
+//         title: "Hello, Redux!",
+//         desc: "Reduxも始めました",
+//         done: false,
+//       },
+//     ];
+//     this.state = {
+//       todos: todos,
+//       countTodo: todos.length + 1,
+//       count: 0
+//     };
+//   }
+
+//   plus() {
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   }
+
+//   minus() {
+//     this.setState({
+//       count: this.state.count - 1
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Nav />
+//         <h1>TODOアプリ作ってみた</h1>
+//         {/* <Form /> */}
+//         <div>count is : {this.state.count}</div>
+//         <button onClick={() => this.plus()}>プラス</button>
+//         <button onClick={() => this.minus()}>マイナス</button>
+//         <List todos={this.state.todos} />
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
